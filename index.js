cb8a44be523230504c694982c511bb604ab075ac
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-01-24 08:45:19 -0800

    Add files

---
+++ index.js
@@ -0,0 +1,23 @@
+
+index.js
+RELOAD BROWSER
+123456
+import React from "react"import ReactDOM from "react-dom" import App from "./App" ReactDOM.render(<App />, document.getElementById("root"))
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+Find
+No Results
+
+CONSOLE
+index.html
+
+Scrimba
+Learn React for free
+Todo App - Phase 7
+Sign in
